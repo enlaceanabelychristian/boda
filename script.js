afe1251c19825439a3068c350c3b5164cc9347ce
enlaceanabelychristian <anabelchristian31@gmail.com>
2026-07-08 11:53:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -251,6 +251,7 @@ if (uploadForm) {
     const formData = new FormData();
     formData.append("file", file);
     formData.append("upload_preset", UPLOAD_PRESET);
+    formData.append("folder", "boda/evento");
 
     try {
       const response = await fetch(`https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`, {
